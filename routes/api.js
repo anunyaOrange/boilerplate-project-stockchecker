c133dabeb27b8fb41b884fa9c30e1d678a0cce4c
anunyaOrange <anunyaOrange@proton.me>
2025-07-10 13:51:49 +0700

    add likeArray

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,24 @@
 
 module.exports = function (app) {
 
+  let likeArray = [{
+    stock: "AMD",
+    likes: new Set(["::ffff:127.0.0.1"])
+  }];
+
   app.route('/api/stock-prices')
-    .get(function (req, res){
+    .get(function (req, res) {
+      const { stock } = req.query;
+      const { like } = req.query;
+      console.log("stock:", stock, "like:", like);
+      console.log("IP", req.ip, req.socket.remoteAddress);
+
+      const stocks = Array.isArray(stock) ? stock : [stock];
+      console.log("stocks", stocks);
+
+
+
       
     });
-    
+
 };
